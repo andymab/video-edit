18adc3059327e194a88ba2ee8ddb1b48440eb76b
Andy mac <650517@355042.ru>
2025-10-25 21:17:21 +0300

    подгрузил mdi

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,12 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-export const vuetify = createVuetify()
+import { aliases, mdi } from 'vuetify/iconsets/mdi'   // ← иконсет MDI
+import '@mdi/font/css/materialdesignicons.css'        // ← CSS со шрифтом-иконками
+
+export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
